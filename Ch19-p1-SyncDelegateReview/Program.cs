fe230f3221a2ed8d00992b39a7f75d61f28146d8
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-11-29 17:23:22 +0500

    Project Created

---
+++ Ch19-p1-SyncDelegateReview/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch19_p1_SyncDelegateReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
